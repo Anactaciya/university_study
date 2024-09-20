7318f9b17e2f2baade984cc0e48b5ab69529aae5
Vega <anastasiya.tsariova@gmail.com>
2024-09-20 21:59:37 +0300

    Отделила печать треугольника в отдельную функцию

--- c_file.cpp
+++ c_file.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
 
-int  main() {
-    int n=0;
-    std::cin >> n;
-    n= n * 2;
+
+void make_triagle(int n) {
+    n = n * 2;
     int c = 0;
     int lc = 0;
     while (c < n) {
-	if (c > n - c) {
-		lc = n - c;
-	}
-	else {
-		lc = c;
-	} 
-	for (int i = 0; i < lc; i++) {
-		std::cout << '*';
-	}
-	std::cout << std::endl;
-    	++c;
-   }
+        if (c > n - c) {
+                lc = n - c;
+        }
+        else {
+                lc = c;
+        } 
+        for (int i = 0; i < lc; i++) {
+                std::cout << '*';
+        }
+        std::cout << std::endl;
+        ++c;
+    }
+}
+
+
+int main() {
+    int n;
+    std::cin >> n;
+    make_triagle(n);
     return 0;
 }
